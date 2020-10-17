8acffedcaff4a35c27380d9d7c2fcb0afcfd26a7
kool nigam <nigamshubham2000@gmail.com>
2020-10-17 22:11:27 +0530

    Learn TL Elvis,If-Unless,Switch-case...

--- src/main/java/com/example/thymeleaf_practice/Controller/MainController.java
+++ src/main/java/com/example/thymeleaf_practice/Controller/MainController.java
@@ -1,20 +1,41 @@
 package com.example.thymeleaf_practice.Controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
-	
+
 	// http://localhost:8080/index
-	@RequestMapping(value = "/index" , method = RequestMethod.GET	)
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index(Model modal) {
-		modal.addAttribute("name","Shubham Nigam");
+		modal.addAttribute("name", "Shubham Nigam");
 		modal.addAttribute("date", new Date());
 		return "index";
+	}
+
+	// Handling Iteration
+	@GetMapping("/iterate")
+	public String iterateHandler(Model model) {
+		List<String> list = List.of("Sakshi", "Shubham", "Shubhanshu");
+		model.addAttribute("friends", list);
+		return "iterate";
+	}
+	
+	// Handler for conditional statements
+	@GetMapping("/condition")
+	public String conditionalHandler(Model model) {
+		model.addAttribute("state", true);
+		model.addAttribute("gender", "F");
+		List<Integer> list = List.of(123,234,345,456);
+		model.addAttribute("mylist",list);
+		return "condition";
 	}
 }
