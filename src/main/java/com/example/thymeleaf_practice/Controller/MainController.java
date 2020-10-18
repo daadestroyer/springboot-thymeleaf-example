26fab51284dcd948a6507999588eb90359a2d1d8
kool nigam <nigamshubham2000@gmail.com>
2020-10-18 12:08:27 +0530

    Learned replace,insert,include

--- src/main/java/com/example/thymeleaf_practice/Controller/MainController.java
+++ src/main/java/com/example/thymeleaf_practice/Controller/MainController.java
@@ -1,5 +1,6 @@
 package com.example.thymeleaf_practice.Controller;
 
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -28,14 +29,27 @@ public class MainController {
 		model.addAttribute("friends", list);
 		return "iterate";
 	}
-	
+
 	// Handler for conditional statements
 	@GetMapping("/condition")
 	public String conditionalHandler(Model model) {
 		model.addAttribute("state", true);
 		model.addAttribute("gender", "F");
-		List<Integer> list = List.of(123,234,345,456);
-		model.addAttribute("mylist",list);
+		List<Integer> list = List.of(123, 234, 345, 456);
+		model.addAttribute("mylist", list);
 		return "condition";
 	}
+
+	// Handler for including fragments
+	@GetMapping("/service")
+	public String service(Model model) {
+		model.addAttribute("title", "This is title ....");
+		model.addAttribute("subtitle","This is subtitle ...");	
+		return "service";
+	}
+	@GetMapping("/newabout")
+	public String newAbout() {
+		return "aboutnew";
+	}
+
 }
